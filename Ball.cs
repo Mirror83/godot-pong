714b8d46c0235ce990edc688268906a16960d7fa
Mirror83 <glenochieng045@gmail.com>
2023-05-29 06:13:08 +0300

    Late initial commit

---
+++ Ball.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System;
+
+public partial class Ball : CharacterBody2D
+{
+    [Export]
+    public int Speed = 200;
+
+    private bool hasLeftScreen = false;
+
+    private Vector2 ballVelocity;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        ballVelocity = new Vector2(GD.RandRange(-1, 1), GD.RandRange(-1, 1));
+        while (
+            ballVelocity == Vector2.Zero ||
+            ballVelocity == Vector2.Up ||
+            ballVelocity == Vector2.Down ||
+            ballVelocity == Vector2.Left ||
+            ballVelocity == Vector2.Right
+          )
+        {
+            ballVelocity = new Vector2(GD.RandRange(-1, 1), GD.RandRange(-1, 1));
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(double delta)
+    {
+        if (Position.X > (GetViewportRect().Size.X))
+        {
+            if (!hasLeftScreen)
+            {
+                GD.Print("Just left the viewport from the right side");
+                hasLeftScreen = true;
+            }
+        }
+        if (Position.X < 0)
+        {
+            if (!hasLeftScreen)
+            {
+                GD.Print("Just left the viewport from the left side");
+                hasLeftScreen = true;
+            }
+        }
+        if (Position.Y > GetViewportRect().Size.Y)
+        {
+            ballVelocity.Y *= -1;
+        }
+        if (Position.Y < 0)
+        {
+            ballVelocity.Y *= -1;
+        }
+
+        var collisionInfo = MoveAndCollide(ballVelocity * Speed * (float)delta);
+        if (collisionInfo != null)
+        {
+            ballVelocity = ballVelocity.Bounce(collisionInfo.GetNormal());
+        }
+    }
+
+
+}
---
+++ Paddle.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+
+public struct PaddleKeyMaps
+{
+    public static string PlayerOneDown = "player_one_down";
+    public static string PlayerOneUp = "player_one_up";
+    public static string PlayerTwoDown = "player_two_down";
+    public static string PlayerTwoUp = "player_two_up";
+
+}
+
+
+public partial class Paddle : StaticBody2D
+{
+
+    [Export]
+    public int Speed = 10;
+    [Signal]
+    public delegate void BallContactEventHandler();
+    public Vector2 ScreenSize;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        ScreenSize = GetViewportRect().Size;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+
+    protected void MoveOnKeyPress(string up, string down)
+    {
+        var velocity = Vector2.Zero;
+        if (Input.IsActionPressed(up))
+        {
+            velocity.Y = -1;
+            Position += velocity * Speed;
+            // Position = new Vector2(
+            //     Math.Clamp(Position.X, 0, ScreenSize.X),
+            //     Math.Clamp(Position.Y, 0, ScreenSize.Y)
+            // );
+        }
+        else if (Input.IsActionPressed(down))
+        {
+            velocity.Y = 1;
+            Position += velocity * Speed;
+            // Position = new Vector2(
+            //     Math.Clamp(Position.X, 0, ScreenSize.X),
+            //     Math.Clamp(Position.Y, 0, ScreenSize.Y)
+            // );
+        }
+    }
+
+
+}
